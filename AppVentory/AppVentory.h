007727dfd0d80d8f0435ab305ccb9e7b13889406
schurik <buss.alexander@gmail.com>
2016-02-09 22:29:49 +0100

    Initial Commit

---
+++ AppVentory/AppVentory.h
@@ -0,0 +1,19 @@
+//
+//  AppVentory.h
+//  AppVentory
+//
+//  Created by Alexander Buss on 09.02.16.
+//  Copyright © 2016 Alexander Buss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppVentory.
+FOUNDATION_EXPORT double AppVentoryVersionNumber;
+
+//! Project version string for AppVentory.
+FOUNDATION_EXPORT const unsigned char AppVentoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVentory/PublicHeader.h>
+
+
